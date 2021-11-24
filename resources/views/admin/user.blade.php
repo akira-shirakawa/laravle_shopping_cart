dec377aac2dab1946ebcc4a172bb59b2cc2c7bd6
akira-shirakawa <akun32310901@yahoo.co.jp>
2021-11-24 20:05:51 +0900

    changed

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -9,17 +9,17 @@
 @section('content')
 <p class="hit">ユーザを検索</p>
 <div  class="search_box">
-    <input  class="input is-rounded ds-input" id="js-input3" type="text" placeholder="Search the docs" >
-    <button id="js-ajax-button" class="button is-rounded" ><i class="fas fa-search"></i></button>
+    <input  class="input is-rounded ds-input" id="js-input3" type="text" placeholder="Search the User" >
+    <!-- <button id="js-ajax-button" class="button is-rounded" ><i class="fas fa-search"></i></button> -->
     
 </div>
 <table class="table ">
-<tr><th>ユーザー名</th><th>作成日</th><th>登録カート個数</th></tr>
+<tr><th>ユーザー名</th><th>作成日</th></tr>
 @foreach($users as $value)
 <tr>
     <td>{{$value->name}}</td>
     <td>{{$value->created_at}}</td>
-    <td>{{$value->carts->count()}}</td>
+   
     <td>
        
 </tr>
